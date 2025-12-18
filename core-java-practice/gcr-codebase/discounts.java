a631173d0146f6535c0d4a809dde11b1e6ed83eb
Rishabh <rachitp25@gmail.com>
2025-12-18 11:37:32 +0530

    Add 3  codes

---
+++ core-java-practice/gcr-codebase/discounts.java
@@ -0,0 +1,9 @@
+public class discounts{
+    public static void main(String[] args) {
+        int fee = 125000;
+        int discountPercent = 10; //discount should be entered in percentage.
+        int discount = (discountPercent/100)*fee;
+        int lastPrice = fee - discount;
+        System.out.println(lastPrice);
+    }
+}
