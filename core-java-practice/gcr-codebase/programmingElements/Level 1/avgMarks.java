0cf57e9679a3d2b93c97282739982903171cf7f8
Rishabh <rachitp25@gmail.com>
2025-12-18 12:48:32 +0530

    2nd and 8th question in  level 1

---
+++ core-java-practice/gcr-codebase/programmingElements/Level 1/avgMarks.java
@@ -0,0 +1,9 @@
+public class avgMarks {
+    public static void main(String[] args) {
+        int p = 95;
+        int m = 94;
+        int c = 96;
+        int avg = (p + m + c) / 3;
+        System.out.println("The average marks of the student is: " + avg);
+    }
+}
---
+++ core-java-practice/gcr-codebase/programmingElements/Level 1/kmToMiles.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class kmToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter distance in kilometers: ");
+        double kilometers = sc.nextDouble();
+        double miles = kilometers * 0.621371;
+        System.out.printf("%.2f kilometers is equal to %.2f miles.%n", kilometers, miles);
+        sc.close();
+    }
+}
