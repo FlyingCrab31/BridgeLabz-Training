6e6b711ccb5d9c82894f3db3a8b43b3e3c1d1312
Rishabh <rachitp25@gmail.com>
2025-12-30 15:46:35 +0530

    Refactor SnakeAndLadder game to support two players and modify movePlayer method to return ladder status.

--- core-java-practice/scenario-based/SnakeAndLadder.java
+++ core-java-practice/scenario-based/SnakeAndLadder.java
@@ -1,5 +1,6 @@
 import java.util.Random;
- public class SnakeAndLadder{
+
+public class SnakeAndLadder {
 
     private static final int WINNING_POSITION = 100;
     private static final int START_POSITION = 0;
@@ -33,8 +34,10 @@ import java.util.Random;
     }
 
     // UC3, UC4, UC5: Move player based on die roll and option
-    public void movePlayer(int dieNumber, int option) {
+    // Modified to return boolean indicating if it was a ladder
+    public boolean movePlayer(int dieNumber, int option) {
         int previousPosition = playerPosition;
+        boolean isLadder = false;
         
         switch (option) {
             case NO_PLAY:
@@ -51,6 +54,7 @@ import java.util.Random;
                     System.out.println("Ladder - Cannot move beyond 100. " + playerPosition);
                 } else {
                     System.out.println("Ladder - Player moves " + playerPosition);
+                    isLadder = true; // UC7: Track that it was a ladder
                 }
                 break;
 
@@ -59,23 +63,24 @@ import java.util.Random;
                 playerPosition -= dieNumber;
 
                 // UC4: player doesn't go below 0, restart from 0
-
                 if (playerPosition < START_POSITION) {
-
                     playerPosition = START_POSITION;
                     System.out.println("Snake - Player moves below 0. " + playerPosition);
                 } else {
                     System.out.println("Snake - Player moves " + playerPosition);
                 }
                 break;
-
         }
+        
+        return isLadder; // UC7: Return if ladder was encountered
     }
-    // UC4, UC5, UC6: Play the complete game
+    
+    // UC4, UC5, UC6: Play the complete game (single player)
     public void playGame() {
         System.out.println(" Game Start");
         System.out.println(" starting position: " + playerPosition);
         System.out.println();
+        
         // UC4: Repeat until player reaches winning position 100
         while (playerPosition != WINNING_POSITION) {
             diceRollCount++;
@@ -91,17 +96,77 @@ import java.util.Random;
             movePlayer(dieNumber, option);
             System.out.println();
         }
+        
         // UC6: Report the number of times dice was played
         System.out.println("Game Over ");
         System.out.println("Player won " + playerPosition);
         System.out.println("Total number of dice rolls: " + diceRollCount);
     }
+    
+    // UC7: Play the game with 2 players
+    public static void playGameWith2Players() {
+        System.out.println(" 2nd Players");
+        System.out.println();
+        
+        // Initialize two players
+        SnakeAndLadder player1 = new SnakeAndLadder();
+        SnakeAndLadder player2 = new SnakeAndLadder();
+        
+        int currentPlayer = 1; // Start with player 1
+        
+        // Game loop
+        while (player1.playerPosition != WINNING_POSITION && 
+               player2.playerPosition != WINNING_POSITION) {
+            
+            SnakeAndLadder activePlayer = (currentPlayer == 1) ? player1 : player2;
+            
+            System.out.println(" Player " + currentPlayer + "'s Turn");
+            System.out.println("Current Position: " + activePlayer.playerPosition);
+            
+            activePlayer.diceRollCount++;
+            
+            // UC2: Roll the die
+            int dieNumber = activePlayer.rollDie();
+            System.out.println("Roll no." + activePlayer.diceRollCount + ": Die no. " + dieNumber);
+
+            // UC3: Check for option
+            int option = activePlayer.checkOption();
+
+            // Move player and check if it was a ladder
+            boolean gotLadder = activePlayer.movePlayer(dieNumber, option);
+            
+            System.out.println();
+            
+            // UC7: If player got a ladder, they play again
+            if (gotLadder) {
+                System.out.println("Player " + currentPlayer + " will Play again!");
+                System.out.println();
+                // same player plays again
+            } else {
+                // Switch to other player
+                currentPlayer = (currentPlayer == 1) ? 2 : 1;
+            }
+        }
+        
+        // UC7: Report which player won the game
+        System.out.println(" GAME OVER ");
+        if (player1.playerPosition == WINNING_POSITION) {
+            System.out.println(" Player 1 WINS! ");
+            System.out.println("Player 1 total dice rolls: " + player1.diceRollCount);
+        } else {
+            System.out.println(" Player 2 WINS!");
+            System.out.println("Player 2 total dice rolls: " + player2.diceRollCount);
+        }
+        
+        System.out.println();
+    }
+    
     // Main method to run the game
     public static void main(String[] args) {
         System.out.println("Snake And Ladder Game!");
         System.out.println();
         
-        SnakeAndLadder game = new SnakeAndLadder();
-        game.playGame();
+        // UC7: Play with 2 players
+        playGameWith2Players();
     }
 }
